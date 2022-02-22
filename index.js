fc197289d5b62d4db6ebe301dcd46c7815947f2a
gargidexterapps <95070453+gargidexterapps@users.noreply.github.com>
2022-02-22 21:34:01 +0530

    role

--- index.js
+++ index.js
@@ -2,6 +2,11 @@ const express = require("express");
 const mongoose = require("mongoose");
 const sessionController = require("./controller/session-controller")
 const roleController=require("./controller/role-controller")
+const userController=require("./controller/user-controller")
+const projectController=require("./controller/project-controller")
+const projectTeamController=require("./controller/project_team-controller")
+const statusController=require("./controller/status-controller")
+const moduleController=require("./controller/project_module-controller")
 const app = express()
 app.use(express.json())
 app.use(express.urlencoded({extended:true}))
@@ -16,8 +21,34 @@ app.get("/login",sessionController.login)
 app.get("/signup",sessionController.signup)
 app.post("/saveuser",sessionController.saveuser)
 app.post("/roles",roleController.addRole)
+app.get("/roles",roleController.getAllRoles)
+app.delete("/roles/:roleId",roleController.deleteRole)
+app.put("/roles",roleController.updateRole)
 
+app.post("/user",userController.addUser)
+app.get("/user",userController.getAllUser)
+app.delete("/user/:UserId",userController.deleteUser)
+app.put("/user",userController.updateUser)
+app.post("/login",userController.login)
 
+app.post("/project",projectController.addProject)
+app.get("/project",projectController.getAllProject)
+app.delete("/project/:ProjectId",projectController.deleteProject)
+app.put("/project",projectController.updateProject)
+
+app.post("/projectteam",projectTeamController.addteam)
+app.get("/projectteam",projectTeamController.getAllProjectTeam)
+app.delete("/projectteam/:ProjectTeamId",projectTeamController.deleteProjectTeam)
+
+app.post("/status",statusController.addStatus)
+app.get("/status",statusController.getStatus)
+app.put("/status",statusController.updatestatus)
+app.delete("/status/:StatusId",statusController.deletestatus)
+
+app.post("/module",moduleController.addmodule)
+app.get("/module",moduleController.getAllModule)
+app.put("/module",moduleController.updatemodule)
+app.delete("/module/:ModuleId",moduleController.deletemodule)
 
 mongoose.connect('mongodb://localhost:27017/timetracking',function(err){
     if(err)
@@ -31,3 +62,4 @@ mongoose.connect('mongodb://localhost:27017/timetracking',function(err){
 app.listen(3000,function(){
     console.log("Server started on 3000");
 })
+                
